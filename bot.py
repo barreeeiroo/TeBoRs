1a5c22b68663874917b44b8c45bf6b04760e064c
Diego Barreiro <diego.barreiro.perez@gmail.com>
2017-01-05 19:20:17 +0100

    Update

--- bot.py
+++ bot.py
@@ -3,33 +3,48 @@ from telebot import types # Bot types API library
 import time # Library for the time
 import feedparser # Imports the feed reader
 import threading # Library for the counter
-from tinydb import TinyDB, where # Library for the database
+import sqlite3 # Library for the database
 
 ###################################################################
 
-#Declare the token
-API_TOKEN = '310239734:AAH5AEFwvGMVTUZzohzYmUHZUnpTrvdTw0Q'
+#Declare the Token
+API_TOKEN = '235906872:AAGqwwYe-TbTzDoGeSxKaC9Fn0A-LzkyRAg'
+
+# Declare the main group
+GROUP_ID = "1057152889"
+
+#Declare the Forum
+FORUM_URL = "community.thunkable.com"
+
+# Declare the Feed
+rss_url = feedparser.parse(FORUM_URL + '/latest.rss')
+
+# Declares the Database
+DATABASE = "feed.db"
+
+###################################################################
+
+conn = sqlite3.connect(DATABASE)
+db = conn.cursor()
+db.execute("CREATE TABLE IF NOT EXISTS latest_topic (id int);")
+conn.commit()
+
+def update_rss():
+    threading.Timer(60.0, update_rss).start()
+    latest_post_online = rss_url['entries'][0]['id'].replace(FORUM_URL + "-topic-", "")
+    print(int(latest_post_online)
+    latest_post_offline = db.execute("SELECT MAX(id) FROM latest_topic;")
+    print(latest_post_offline.fetchone())
+    #if latest_post_online > latest_post_offline:
+    #    db.execute("INSERT INTO latest_topic (id) VALUES (" + latest_post_online + ")")
+    #    conn.commit()
+    #    send_updates()
+update_rss()
+
+###################################################################
 
 # Makes the bot
 bot = telebot.TeleBot(API_TOKEN)
-
-# Get the RSS
-rss_url = feedparser.parse('http://community.thunkable.com/latest.rss')
-last_post = rss_url['entries'][0]['id'].replace("community.thunkable.com-topic-","")
-
-# Declares the database
-db = TinyDB('feed.json') # Use this database
-table = db.table('rss-feed') # Use this table
-#int(latest_post_offline) = table.search(where ('latest_post') > 1)
-#print(latest_post_offline, a)
-#table.insert({'latest_post': last_post})
-
-#def check_community_feed():
-#    threading.Timer(60.0, check_community_feed).start()
-#    last_post_online = rss_url['entries'][0]['id']
-#check_community_feed()
-
-###################################################################
 
 #Listener
 def listener(messages):
@@ -44,15 +59,15 @@ bot.set_update_listener(listener)
 
 # Handle '/start'
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
+def start(message):
     bot.reply_to(message, """\
 Hi there, I the Official Bot of the [Thunkable Community](http://community.thunkable.com).
 Check what I can do using the command /help!\
 """, parse_mode="markdown")
 
-# Handle '/latest_post'
-@bot.message_handler(commands=['latest_post', 'latest_topic'])
-def latest_post(message):
+# Handle /latest_topic
+@bot.message_handler(commands=['latest_topic'])
+def latest_topic(message):
     bot.reply_to(message, """*This is the latest public post in the community*\n\n""" +
     "*Title:* " + rss_url['entries'][0]['title'] + "\n" +
     "*Category:* " + rss_url['entries'][0]['category'] + "\n" +
@@ -60,10 +75,28 @@ def latest_post(message):
     "_See it _[here](" + rss_url['entries'][0]['link'] + ")",
     parse_mode="markdown")
 
+# Handle /latest_post
+@bot.message_handler(commands=['latest_post'])
+def latest_post(message):
+    bot.reply_to(message, """
+I am working on this command, sorry\n
+But you can use /latest\_topic to get the latest conversation in the community
+    """,
+    parse_mode="markdown")
+
 #Handle /ping
 @bot.message_handler(commands=['ping'])
-def update_rss(message):
+def ping(message):
     bot.reply_to(message, "*p*_o_`n`g", parse_mode="markdown")
+
+# Send forum update
+def send_updates():
+    bot.send_message(GROUP_ID, "*New post in the community:* \n\n"
+"*Title:* " + rss_url['entries'][0]['title'] + "\n" +
+"*Category:* " + rss_url['entries'][0]['category'] + "\n" +
+"*Author:* " + rss_url['entries'][0]['author'] + "\n\n" +
+"_See it _[here](" + FORUM_URL + "/t/" + latest_post_online + ")",
+    parse_mode="markdown")
 
 ###################################################################
 
